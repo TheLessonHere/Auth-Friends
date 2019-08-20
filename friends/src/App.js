442b8006a0d13f23ca7baef7a535c477167bdb34
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-08-20 14:09:20 -0600

    Routes added, empty components created

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
 import './App.css';
+
+import LoginForm from './components/LoginForm';
+import FriendsList from './components/Friends';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+      <div className="App">
+      <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={LoginForm} />
+        <PrivateRoute exact path="/protected" component={FriendsList} />
+      </div>
   );
 }
 
